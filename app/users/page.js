5f47453721c15c7518129bb9173c8b5a762ca35d
mansinakrani <mansinakrani006@gmail.com>
2024-02-15 17:50:59 +0530

    generateMetadata for specific page

---
+++ app/users/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <div>
+      Users
+    </div>
+  )
+}
+
+export default Users;
+
+export const generateMetadata = () => {
+  return {
+    title: "Users Details Page",
+    description: "All about user details",
+  };
+}
